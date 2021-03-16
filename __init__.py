90076164327d3a3849fd16e434ec3ae1f9273f3a
ngaglani <ngaglani@splunk.com>
2021-03-16 16:50:02 +0530

    Updating the license of the init file

--- __init__.py
+++ __init__.py
@@ -2,6 +2,4 @@
 # Copyright (c) 2018-2021 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.# File: __init__.py
-#
-# --
+# without a valid written license from Splunk Inc. is PROHIBITED.
