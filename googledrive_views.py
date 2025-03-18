a05ebedcae6802170df35cd9b7489f7c1c125ba1
Ian Rokas <grokas@splunk.com>
2025-03-18 03:23:12 +0000

    chore(ci): workflow updates and pre-commit changes and configurations added and applied
    [batch update]

--- googledrive_views.py
+++ googledrive_views.py
@@ -1,6 +1,6 @@
 # File: googledrive_views.py
 #
-# Copyright (c) 2018-2023 Splunk Inc.
+# Copyright (c) 2018-2025 Splunk Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,28 +13,27 @@
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 def list_users(provides, all_results, context):
+    context["results"] = results = []
 
-    context['results'] = results = []
-
-    headers = ['Status', 'Full Name', 'Email']
-    context['headers'] = headers
+    headers = ["Status", "Full Name", "Email"]
+    context["headers"] = headers
 
     for summary, action_results in all_results:
         for result in action_results:
             table = dict()
-            table['data'] = rows = []
+            table["data"] = rows = []
             data = result.get_data()
 
-            result_status = 'failed'
+            result_status = "failed"
             if result.get_status():
-                result_status = 'success'
+                result_status = "success"
 
             for item in data:
                 row = []
-                row.append({'value': result_status})
-                row.append({'value': item.get('name', {}).get('fullName')})
-                row.append({'value': item.get('primaryEmail'), 'contains': ['email']})
+                row.append({"value": result_status})
+                row.append({"value": item.get("name", {}).get("fullName")})
+                row.append({"value": item.get("primaryEmail"), "contains": ["email"]})
                 rows.append(row)
             results.append(table)
 
-    return 'googledrive_list_users.html'
+    return "googledrive_list_users.html"
